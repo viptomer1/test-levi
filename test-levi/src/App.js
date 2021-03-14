07474a54a2b84f3834ad13ce9d9e3a60e9b6face
vip1 <vipin_uiet@yahoo.com>
2021-03-14 18:05:12 +0000

    VT:test created with two screens routing

--- test-levi/src/App.js
+++ test-levi/src/App.js
@@ -3,29 +3,29 @@ import './App.css';
 
 
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Product from './components/Product';
 
 class App extends Component { //TODO add font image for back button
   render() {
-    return ( <div className="App">my adpp</div>
-    // <Router>
-    //     <div>
-    //       {/* <h2>Welcome to React Router Tutorial</h2>
-    //       <nav className="navbar navbar-expand-lg navbar-light bg-light">
-    //       <ul className="navbar-nav mr-auto">
-    //         <li><Link to={'/'} className="nav-link"> Home </Link></li>
-    //         <li><Link to={'/product'} className="nav-link">Prod</Link></li>
-    //       </ul>
-    //       </nav>
-    //       <hr /> */}
-    //       <Switch>
-    //           <Route exact path='/' component={Home} />
-    //           <Route path='/product' component={Product} />
-    //       </Switch>
-    //     </div>
-    //   </Router>
+    return (
+    <Router>
+        <div>
+          {/* <h2>Welcome to React Router Tutorial</h2>
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <ul className="navbar-nav mr-auto">
+            <li><Link to={'/'} className="nav-link"> Home </Link></li>
+            <li><Link to={'/product'} className="nav-link">Prod</Link></li>
+          </ul>
+          </nav>
+          <hr /> */}
+          <Switch>
+              <Route exact path='/' component={Home} />
+              <Route path='/product' component={Product} />
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
--- test-levi/src/components/ProductItem.js
+++ test-levi/src/components/ProductItem.js
@@ -1,46 +1,46 @@
-// import React from "react";
-// import { useHistory } from "react-router-dom";
+import React from "react";
+import { useHistory } from "react-router-dom";
 
-// const ProductItem = props => { //console.log('Item>>>>>>',props);
-//   const { product } = props;
-//   const { context } = props;
-//   const history = useHistory();
+const ProductItem = props => { //console.log('Item>>>>>>',props);
+  const { product } = props;
+  const { context } = props;
+  const history = useHistory();
 
-//   //TODO Method for selected id 
-//   function selectProduct(selId) {
-//     context.selectProductId = selId;
-//     history.push("/product" , context);
-//   }
-//   return (
-//     <div className="card column is-half">
-//       <div className="box">
-//         <div className="media">
-//           <div className="media-left">
-//               <img className="prodItemsImg"
-//                 src={product.image}
-//                 alt={product.title}
-//                 onClick={() =>
-//                     context.history.push({
-//                         pathname: '/product',
-//                         state: {selectedId: product.productId}
-//                     })}
-//               />
-//           </div>
+  //TODO Method for selected id 
+  function selectProduct(selId) {
+    context.selectProductId = selId;
+    history.push("/product" , context);
+  }
+  return (
+    <div className="card column is-half">
+      <div className="box">
+        <div className="media">
+          <div className="media-left">
+              <img className="prodItemsImg"
+                src={product.image}
+                alt={product.title}
+                onClick={() =>
+                    context.history.push({
+                        pathname: '/product',
+                        state: {selectedId: product.productId}
+                    })}
+              />
+          </div>
 
-//           <div className="media-content">
+          <div className="media-content">
             
-//             <div>{product.title}</div>
-//             <b style={{ textTransform: "capitalize" }}>
-//               {product.name}{" "}
-//               <span className="tag is-primary">£{product.price.now}</span>
-//             </b>
+            <div>{product.title}</div>
+            <b style={{ textTransform: "capitalize" }}>
+              {product.name}{" "}
+              <span className="tag is-primary">£{product.price.now}</span>
+            </b>
             
-//           </div>
+          </div>
 
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
+        </div>
+      </div>
+    </div>
+  );
+};
 
-// export default ProductItem;
+export default ProductItem;
